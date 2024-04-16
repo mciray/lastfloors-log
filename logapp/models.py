077684c9ee10f9b27ad857bbef798641651ef08d
mciray <melihciray@gmail.com>
2024-04-16 15:11:47 +0300

    extra fields added

--- logapp/models.py
+++ logapp/models.py
@@ -5,7 +5,14 @@ class LogEntry(models.Model):
     method = models.CharField(max_length=10)
     status_code = models.IntegerField()
     user = models.CharField(max_length=255, default='Anonymous')
-    response_time= models.FloatField(blank=True)
+    response_time = models.FloatField(blank=True)
+    ip_address = models.CharField(max_length=45, blank=True)
+    query_params = models.JSONField(blank=True, null=True)
+    exception = models.TextField(blank=True)
+    traceback = models.TextField(blank=True)
+    file_name = models.CharField(max_length=255, blank=True)
+    line_number = models.IntegerField(blank=True, null=True)
+    function_name = models.CharField(max_length=255, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
